19c5db04496acd48d2056bb9bc2c834350eb7974
Otávio Paganotti <paganottiotavio@gmail.com>
2019-11-10 14:18:56 -0400

    implementando leitura de arquivos

--- src/main/java/controller/ArenaController.java
+++ src/main/java/controller/ArenaController.java
@@ -10,5 +10,42 @@ package controller;
  * @author otavi
  */
 public class ArenaController {
+    private int altura;
+    private int largura;
+    private int comprimento;
+
+    public ArenaController(int altura, int largura, int comprimento) {
+        this.altura = altura;
+        this.largura = largura;
+        this.comprimento = comprimento;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public int getLargura() {
+        return largura;
+    }
+
+    public void setLargura(int largura) {
+        this.largura = largura;
+    }
+
+    public int getComprimento() {
+        return comprimento;
+    }
+
+    public void setComprimento(int comprimento) {
+        this.comprimento = comprimento;
+    }    
     
+    public void main() {
+        ArenaController arena = new ArenaController(10, 10, 10);
+        System.out.println(arena.getAltura());
+    }
 }
---
+++ src/main/java/view/main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import controller.ArenaController;
+
+/**
+ *
+ * @author otavi
+ */
+public class main {
+  ArenaController arena = new ArenaController(10, 10, 10);
+  public static void main(String[] args) {
+      
+  }
+}
