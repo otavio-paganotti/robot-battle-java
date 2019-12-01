83c347e112df93ddb9a246110b1566ac3c9e6d91
Otávio Paganotti <otavio@dourasoft.com.br>
2019-12-01 15:21:08 -0400

    refactoring project

--- src/main/java/controller/ItensEspeciais.java
+++ src/main/java/controller/ItensEspeciais.java
@@ -3,9 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package model;
+package controller;
 
 import java.util.ArrayList;
+import model.Arma;
+import model.Bomba;
+import model.Virus;
 
 /**
  *
